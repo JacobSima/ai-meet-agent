9c5dea09e4849a55a105be8cf74cbc5f10086be4
Jacob K Sima <simajacob2011@gmail.com>
2025-06-13 20:02:11 +0200

    06-dashboard-sidebar

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,19 @@
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { DashboardSideBar } from "@/modules/dashboard/ui/components/dashboard-sidebar";
+
+interface Props {
+  children: React.ReactNode
+}
+
+const Layout = ({ children }: Props) => {
+  return (
+    <SidebarProvider>
+      <DashboardSideBar />
+      <main className="flex flex-col h-screen w-screen bg-muted">
+        {children}
+      </main>
+    </SidebarProvider>
+  );
+};
+
+export default Layout;
